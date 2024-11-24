f44d018295f3f48684041f5d9d99a8e1c339c3ee
Joseph H.T. Nguyen <164699475+Nguyenjh-24@users.noreply.github.com>
2024-11-23 21:14:51 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/Upcycling-and-Waste-Management",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
